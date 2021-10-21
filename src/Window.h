251369a006cb5b32b20e637ce5dd5d2dd17c442c
boom-roasted <noah.prezant@protonmail.ch>
2021-10-21 00:23:21 -0600

    Move Window definitions to code file

--- src/Window.h
+++ src/Window.h
@@ -8,55 +8,20 @@
 class Window
 {
 public:
-    explicit Window(SDL_Window* window)
-        : window_(window)
-    {
-    }
+    explicit Window(SDL_Window* window);
+    
+    Window(const char* title, int x, int y, int w, int h, Uint32 flags);
+    Window(Window&& other) noexcept;
 
-    Window(const char* title, int x, int y, int w, int h, Uint32 flags)
-    {
-        window_ = SDL_CreateWindow(title, x, y, w, h, flags);
-        if (window_ == nullptr)
-        {
-            SDL_LogCritical(
-                SDL_LOG_CATEGORY_VIDEO,
-                "Window could not be created! SDL Error: %s\n",
-                SDL_GetError());
-            SDL_assert(window_ != nullptr);
-        }
-    }
-
-    Window(Window&& other) noexcept
-        : window_(other.window_)
-    {
-        other.window_ = nullptr;
-    }
-
-    Window& operator=(Window&& other) noexcept
-    {
-        if (&other == this)
-            return *this;
-        if (window_ != nullptr)
-            SDL_DestroyWindow(window_);
-        window_ = other.window_;
-        other.window_ = nullptr;
-        return *this;
-    }
+    Window& operator=(Window&& other) noexcept;
 
     // Window is not copyable
     Window(const Window& other) = delete;
     Window& operator=(const Window& other) = delete;
 
-    ~Window() { dispose(); }
+    ~Window();
 
-    void dispose()
-    {
-        if (window_ != nullptr)
-        {
-            SDL_DestroyWindow(window_);
-            window_ = nullptr;
-        }
-    }
+    void dispose();
 
     // Cast operator for using this class with the SDL library functions
     operator SDL_Window*() { return window_; }
