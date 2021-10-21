19db5e2d706c1e6f8b64095271d7bdc93dca8fc9
boom-roasted <noah.prezant@protonmail.ch>
2021-10-21 00:14:21 -0600

    Move Renderer definitions to code file

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,1 +1,52 @@
 #include "Renderer.h"
+
+Renderer::Renderer(SDL_Renderer* renderer)
+    : renderer_(renderer)
+{
+}
+
+Renderer::Renderer(SDL_Window* window, int index, Uint32 flags)
+{
+    renderer_ = SDL_CreateRenderer(window, index, flags);
+    if (renderer_ == nullptr)
+    {
+        SDL_LogCritical(
+            SDL_LOG_CATEGORY_VIDEO,
+            "Renderer could not be created! SDL Error: %s\n",
+            SDL_GetError());
+        SDL_assert(renderer_ != nullptr);
+    }
+}
+
+Renderer::Renderer(Renderer&& other) noexcept
+    : renderer_(other.renderer_)
+{
+    other.renderer_ = nullptr;
+}
+
+Renderer&
+Renderer::operator=(Renderer&& other) noexcept
+{
+    if (&other == this)
+        return *this;
+    if (renderer_ != nullptr)
+        SDL_DestroyRenderer(renderer_);
+    renderer_ = other.renderer_;
+    other.renderer_ = nullptr;
+    return *this;
+}
+
+Renderer::~Renderer()
+{
+    dispose();
+}
+
+void
+Renderer::dispose()
+{
+    if (renderer_ != nullptr)
+    {
+        SDL_DestroyRenderer(renderer_);
+        renderer_ = nullptr;
+    }
+}
--- src/Renderer.h
+++ src/Renderer.h
@@ -8,55 +8,21 @@
 class Renderer
 {
 public:
-    explicit Renderer(SDL_Renderer* renderer)
-        : renderer_(renderer)
-    {
-    }
+    explicit Renderer(SDL_Renderer* renderer);
 
-    Renderer(SDL_Window* window, int index, Uint32 flags)
-    {
-        renderer_ = SDL_CreateRenderer(window, index, flags);
-        if (renderer_ == nullptr)
-        {
-            SDL_LogCritical(
-                SDL_LOG_CATEGORY_VIDEO,
-                "Renderer could not be created! SDL Error: %s\n",
-                SDL_GetError());
-            SDL_assert(renderer_ != nullptr);
-        }
-    }
+    Renderer(SDL_Window* window, int index, Uint32 flags);
 
-    Renderer(Renderer&& other) noexcept
-        : renderer_(other.renderer_)
-    {
-        other.renderer_ = nullptr;
-    }
+    Renderer(Renderer&& other) noexcept;
 
-    Renderer& operator=(Renderer&& other) noexcept
-    {
-        if (&other == this)
-            return *this;
-        if (renderer_ != nullptr)
-            SDL_DestroyRenderer(renderer_);
-        renderer_ = other.renderer_;
-        other.renderer_ = nullptr;
-        return *this;
-    }
+    Renderer& operator=(Renderer&& other) noexcept;
 
     // Renderer is not copyable
     Renderer(const Renderer& other) = delete;
     Renderer& operator=(const Renderer& other) = delete;
 
-    ~Renderer() { dispose(); }
+    ~Renderer();
 
-    void dispose()
-    {
-        if (renderer_ != nullptr)
-        {
-            SDL_DestroyRenderer(renderer_);
-            renderer_ = nullptr;
-        }
-    }
+    void dispose();
 
     // Cast opertator. Converts the C++ wrapper object into C object
     // when it is used with the C functions that come with the library
